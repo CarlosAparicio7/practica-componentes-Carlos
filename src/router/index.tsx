e8f27f91056f4b9acbcaf5b8cda1b65e39f55868
Iván <ivan.corrales@wesovilabs.com>
2025-12-03 10:29:26 +0100

    Fix issue

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,3 @@
-import DetalleAlumno from "@/pages/DetalleAlumno.tsx";
-import ListadoAlumnos from "@/pages/ListadoAlumnos.tsx";
-import PageNotFound from "@/pages/PageNotFound.tsx";
 import {createBrowserRouter} from "react-router-dom";
 
 
